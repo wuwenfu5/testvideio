5d50e04d93a1dd29701076ad56b5cbdd58158131
wuwenfu5 <1442063446@qq.com>
2018-04-17 22:42:16 +0800

    @Mr.Five

--- testflow.py
+++ testflow.py
@@ -8,8 +8,25 @@
 """
 
 import tensorflow as tf
+import numpy as np
 
+x_data = np.random.rand(100).astype("float32")
+y_data = x_data * 0.1 + 0.3
 
-hello = tf.constant('Hello, TensorFlow!')
+W = tf.Variable(tf.random_uniform([1], 1.0, 1.0))
+b = tf.Variable(tf.zeros([1]))
+y = W * x_data + b
+
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
 sess = tf.Session()
-print(sess.run(hello))
+sess.run(init)
+
+for step in range(201):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(W), sess.run(b))
