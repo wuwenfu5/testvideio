63b05b9cf19a8ab9c3671539e3f21ccf43542273
wuwenfu5 <1442063446@qq.com>
2018-05-08 20:20:06 +0800

    @Mr.Five

--- testv.py
+++ testv.py
@@ -10,13 +10,12 @@
 import numpy as np
 import cv2
 import time
-import matlab
+import os
 
-cap = cv2.VideoCapture(r'/home/wuwenfu5/PycharmProjects/Three_people_cross.mp4')
-# cap = cv2.VideoCapture(r'/media/wuwenfu5/Win&Ubuntu/Python_/Material/Fast_wending.mp4')
-# cap = cv2.VideoCapture(r'/media/wuwenfu5/Win&Ubuntu/Python_/Material/DJI_4.mp4')
-# cap = cv2.VideoCapture(r'/media/wuwenfu5/Win&Ubuntu/Python_/Material/swimming03.mp4')
-
+# cap = cv2.VideoCapture(r'/media/wuwenfu5/Win_Ubuntu_Swap/Python_/Material/MyMOT/shitang10.AVI')
+cap = cv2.VideoCapture(r'/media/wuwenfu5/Win_Ubuntu_Swap/Python_/Material/Many.mp4')
+body_cascade = cv2.CascadeClassifier(
+    r'/usr/local/share/OpenCV/haarcascades/haarcascade_fullbody.xml')
 # cap = cv2.VideoCapture(0)
 
 
@@ -28,7 +27,8 @@ else:
 kernel3x3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 kernel6x6 = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4))
 
-fgbg = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=16, detectShadows=True)
+fgbg = cv2.createBackgroundSubtractorMOG2(history=200, varThreshold=32, detectShadows=True)
+# fgbg = cv2.createBackgroundSubtractorKNN(history=200, detectShadows=True)
 
 params = cv2.SimpleBlobDetector_Params()
 # Change thresholds
@@ -43,8 +43,8 @@ params.blobColor = 255
 
 # Filter by Area. 面积
 params.filterByArea = True
-params.minArea = 200
-params.maxArea = 2500
+params.minArea = 150
+params.maxArea = 50000
 
 # Filter by Circularity 圆度
 params.filterByCircularity = False
@@ -59,7 +59,7 @@ params.filterByInertia = False
 params.minInertiaRatio = 0.01
 
 time_last = -1.0  # 避免刚运行程序时时间差为0，
-frame_count = 0
+frame_count = 1
 
 time_kaishi = time.perf_counter()
 
@@ -71,8 +71,10 @@ print('fps=', fps_)
 print('frames= ', frames)
 print('frames_wxh=', frames_w, 'x', frames_w)
 
-tracker = cv2.MultiTracker_create()
+# tracker = cv2.MultiTracker_create()
 
+results = []
+detections_out = []
 while cap.isOpened():
     ret, frame = cap.read()
 
@@ -95,6 +97,24 @@ while cap.isOpened():
         ret, binary = cv2.threshold(fgmask, 128, 255, cv2.THRESH_BINARY)
         binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel6x6, iterations=2)
 
+        # Copy the thresholded image.
+        im_floodfill = binary.copy()
+
+        # Mask used to flood filling.
+        # Notice the size needs to be 2 pixels than the image.
+        h, w = binary.shape[:2]
+        mask = np.zeros((h + 2, w + 2), np.uint8)
+
+        # Floodfill from point (0, 0)
+        cv2.floodFill(im_floodfill, mask, (0, 0), 255)
+
+        # Invert floodfilled image
+        im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+
+        # Combine the two images to get the foreground.
+        binary = binary | im_floodfill_inv
+
+
         detector = cv2.SimpleBlobDetector_create(params)
         keypoints = detector.detect(binary)
         frame_RGB = cv2.cvtColor(binary, cv2.COLOR_GRAY2RGB)
@@ -107,17 +127,19 @@ while cap.isOpened():
             y = int(keypoints[index].pt[1])
             w = h = int(keypoints[index].size)
 
-            if frame_count % 10 == 0:
-                roi_x1 = x - int(0.9 * w)
-                roi_x2 = x + int(0.9 * w)
-                roi_y1 = y - int(1.6 * h)
-                roi_y2 = y + int(1.6 * h)
-                if roi_x1 > 0 and roi_x2 > 0 and roi_y1 > 0 and roi_y2 > 0:
-                    roi = frame[roi_y1: roi_y2, roi_x1: roi_x2]
-                    cv2.imshow('ROI', roi)
-                    cv2.imwrite(str('./logs/img_%d_%d.png' % (frame_count, roi_count)), roi)
-                roi_count += 1
+            # if frame_count % 10 == 0:
+            roi_x1 = x - int(0.9 * w)
+            roi_x2 = x + int(0.9 * w)
+            roi_y1 = y - int(1.6 * h)
+            roi_y2 = y + int(1.6 * h)
+            if roi_x1 > 0 and roi_x2 > 0 and roi_y1 > 0 and roi_y2 > 0:
+                roi = frame[roi_y1: roi_y2, roi_x1: roi_x2]
+                # cv2.imshow('ROI', roi)
+                # cv2.imwrite(str('./logs/img_%d_%d.png' % (frame_count, roi_count)), roi)
+            roi_count += 1
 
+            results.append([frame_count, index, roi_x1, roi_y1, int(1.8 * w), int(3.2 * h)])
+            detections_out += [np.r_[(frame_count, index, roi_x1, roi_y1, int(1.8 * w), int(3.2 * h), 1, -1, -1, -1)]]
             cv2.rectangle(frame, (x - int(0.6 * w), y - int(1.2 * h)), (x + int(0.6 * w), y + int(1.2 * h)),
                           (0, 255, 0), 1)
 
@@ -152,6 +174,12 @@ while cap.isOpened():
         # ret, frame = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
         # frame = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
         #                               cv2.THRESH_BINARY, 11, 2)
+
+        # bodys = body_cascade.detectMultiScale(gray, 1.3, 3)
+        # print(bodys)
+        # for (x, y, w, h) in bodys:
+        #     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
         cv2.imshow('Original', frame)
         cv2.moveWindow('Original', 100, 0)
         cv2.imshow('Closing And Blob', frame_RGB)
@@ -162,6 +190,15 @@ while cap.isOpened():
         break
 
 print('process time:%0.3f' % (time.perf_counter() - time_kaishi))
+# print(results)
+# Store results.
+f = open('./logs/det.txt', 'w')
+for row in results:
+    print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1' % (
+        row[0], row[1], row[2], row[3], row[4], row[5]), file=f)
 
+# output_filename = os.path.join('./logs', "det.npy")
+# np.save(output_filename, np.asarray(detections_out), allow_pickle=False)
+# print(detections_out)
 cap.release()
 cv2.destroyAllWindows()
